6a8034c6ea1f9200220202a2ec10aa43693a83c6
Mert Buldur <email@gmail.com>
2018-08-09 13:55:22 +0300

    Fixed

--- index.php
+++ index.php
@@ -1,7 +1,22 @@
 <?php
-// 0: Single Mysql , 1 : Single Site , 2 : Full Back Up
+/*
+ *  Options Start Type
+ *  0 : Single Mysql Backup
+ *  1 : Single Site Backup
+ *  2 : Mysql and Site Backup
+ */
+
+/*
+ * if you want delete old backup file you must use ->deleteDay parameter
+ * if you want site backup block files , you must use ->blocklist array
+ */
 include "Backup.php";
 $backup = new Backup("localhost","","root","");
 $backup->deleteDay = 2;
 $backup->blockList = ['.DS_Store','.idea',$_SERVER['DOCUMENT_ROOT'].'/.',$_SERVER['DOCUMENT_ROOT'].'/..',];
+
+
+
+
+
 $backup->start(2);
